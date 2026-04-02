2a333654da5c9063fc0b1894550aaa695eda7ffc
ask861 <71695740+ask861@users.noreply.github.com>
2026-04-02 15:19:36 +0800

    Add files via upload

---
+++ Varaible1.java
@@ -0,0 +1,14 @@
+package variable;
+
+public class Varaible1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 20;
+		String name = "Taro";
+		
+		System.out.println("私は"+ name + "です。年齢は" + age + "歳です。");
+
+	}
+
+}
